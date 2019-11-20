afb58c81d8cb0ca55b1f5d6994c8d6cef2a2cc33
Jeisson Guevara <guevara@civicplus.com>
2019-11-20 18:04:24 -0500

    Starting with the DOM tree to draw UI stuff.

---
+++ CoreUI.Dom/CoreUIDomNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace CoreUI.Dom
+{
+    public abstract class CoreUIDomNode
+    {
+        public CoreUIDomNode Parent { get; set; }
+
+        /// <summary>
+        /// Position relative to parent
+        /// </summary>
+        public Point Position { get; set; } = new Point();
+
+        public abstract Size Size { get; set; }
+
+        IList<CoreUIDomNode> Children { get; } = new List<CoreUIDomNode>();
+    }
+}
